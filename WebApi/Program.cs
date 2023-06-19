208f1a3d3b675a2eb60ce5ab8ebaed87d8e83a59
NurluhanKakpan <nurluhankakpan@gmail.com>
2023-06-19 20:32:54 +0600

    adding cors

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -13,10 +13,21 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 builder.Services.AddPersistence(builder.Configuration);
-var app = builder.Build();
 
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+    option =>
+    {
+        option
+            .WithOrigins("http://localhost:3000")                        
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowCredentials();
+    }));  
+
+var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
